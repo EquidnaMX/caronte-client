01ceb3f5cb66ae63a1463a2f605f77a5fe1256db
gruelas <gruelasjr@gmail.com>
2024-09-18 18:07:42 -0600

    Update CaronteController.php

--- src/Http/Controllers/CaronteController.php
+++ src/Http/Controllers/CaronteController.php
@@ -9,7 +9,7 @@
 namespace Equidna\Caronte\Http\Controllers;
 
 use Illuminate\Routing\Controller;
-use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\View;
 use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Response;
