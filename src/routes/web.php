17b6b193700ca6a527b8a7cefefd0813aa1896f1
gruelas <gruelasjr@gmail.com>
2024-12-12 18:35:29 -0600

    1.1.2

--- src/routes/web.php
+++ src/routes/web.php
@@ -19,10 +19,14 @@ Route::get('/2fa/{token}', [CaronteController::class, 'twoFactorTokenLogin']);
 
 Route::match(['get', 'post'], 'logout', [CaronteController::class, 'logout'])->name('caronte.logout');
 
-Route::get('password/recover', [CaronteController::class, 'passwordRecoverRequestForm'])->name('caronte.password.recover');
-Route::post('password/recover', [CaronteController::class, 'passwordRecoverRequest']);
-Route::get('password/recover/{token}', [CaronteController::class, 'passwordRecoverTokenValidation']);
-Route::post('password/recover/{token}', [CaronteController::class, 'passwordRecover']);
+Route::prefix('password/recover')->group(
+    function () {
+        Route::get('', [CaronteController::class, 'passwordRecoverRequestForm'])->name('caronte.password.recover');
+        Route::post('', [CaronteController::class, 'passwordRecoverRequest']);
+        Route::get('{token}', [CaronteController::class, 'passwordRecoverTokenValidation']);
+        Route::post('{token}', [CaronteController::class, 'passwordRecover']);
+    }
+);
 
 Route::get('get-token', [CaronteController::class, 'getToken'])->name('caronte.token.get')
     ->middleware(
